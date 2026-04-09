bde7e0afbfe28b98cbe50a1905119b14c7f21c75
Tonia2006 <114807596+Tonia2006@users.noreply.github.com>
2026-04-09 14:28:25 +0300

    test

---
+++ script.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const schemes=[
+
+    ]
+
+    function addScheme(){
+        const name = document.getElementById("name")
+        const img = document.getElementById("image")
+        const type = document.getElementById("type")
+
+        const scheme = {
+            name: name,
+            img: img,
+            type: type
+        }
+
+        schemes.push(scheme)
+        create_block(scheme)
+    }
+
+    const add = document.querySelector(".create")
+    add.addEventListener("click", addScheme)
+
+    const library = document.querySelector(".library")
+
+    function create_block(scheme){
+        const block = document.createElement('div')
+        block.classList.add('card')
+
+        const img = document.createElement('img')
+        img.src = scheme['img']
+        img.classList.add("img-html")
+
+        const name = document.createElement('h3')
+        name.classList.add('name')
+        name.textContent = scheme.name
+
+        const type = document.createElement('p')
+        type.classList.add("type")
+        type.textContent = "Тип: " + scheme.type
+
+        block.appendChild(name)
+        block.appendChild(img)
+        block.appendChild(type)
+        
+        library.appendChild(block)
+    }
+    
+    for (let i = 0; i < schemes.length; i += 1) {
+        create_block(schemes[i])
+    }
+})
